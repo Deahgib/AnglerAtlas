e851911330f094679ad81b341f5908ea2b9c16cb
Louis <louis.bennette@gmail.com>
2025-03-11 12:33:01 +0000

    Updated the tabs with a cool new look.

---
+++ src/AnglerAtlas/UI/RightSideInformationPanel.lua
@@ -0,0 +1,47 @@
+local RightSideInformationPanel = {}
+
+local UI = AnglerAtlas.MM:GetModule("UI")
+
+local TabManager = AnglerAtlas.MM:GetModule("TabManager")
+local ZoneInfo = AnglerAtlas.MM:GetModule("ZoneInfo")
+local Resipes = AnglerAtlas.MM:GetModule("Resipes")
+local Equipment = AnglerAtlas.MM:GetModule("Equipment")
+
+function RightSideInformationPanel:Create(uiParent)
+
+    local container = CreateFrame("FRAME", "angler-right-side-information-panel", uiParent)
+    container:SetSize(300, 408)
+    container:SetPoint("TOPLEFT", UI.info, "TOPRIGHT", 5, 0)
+    
+    local tabManager = TabManager:Create()
+
+
+    -- Make the zone info panel
+    local zoneInfo = ZoneInfo:Create(container)
+
+    -- Make the recipes panel
+    local resipesUI = Resipes:Create(container, zoneInfo)
+
+    -- Make the equipment panel
+    local equipmentUI = Equipment:Create(container, zoneInfo)
+
+    -- Make the tab buttons
+    local defaultToggleButton = tabManager:CreateTabButton("default", container, '203750', "Zone Info")
+    defaultToggleButton:SetPoint("BOTTOMLEFT", container, "TOPLEFT", 12, -3)
+
+    local recipesToggleButton = tabManager:CreateTabButton("recipes", container, '21742', "Recipes")
+    recipesToggleButton:SetPoint("LEFT", defaultToggleButton, "RIGHT", -5, 0)
+
+    local equipmentToggleButton = tabManager:CreateTabButton("equipment", container, '19972', "Equipment")
+    equipmentToggleButton:SetPoint("LEFT", recipesToggleButton, "RIGHT", -5, 0)
+
+    -- Register the tabs
+    tabManager:Register('default', defaultToggleButton, zoneInfo)
+    tabManager:Register('recipes', recipesToggleButton, resipesUI)
+    tabManager:Register('equipment', equipmentToggleButton, equipmentUI)
+
+    tabManager:Select('default')
+
+end
+
+AnglerAtlas.MM:RegisterModule("RightSideInformationPanel", RightSideInformationPanel)
--- src/AnglerAtlas/UI/TabManager.lua
+++ src/AnglerAtlas/UI/TabManager.lua
@@ -4,7 +4,7 @@ function TabManager:Create()
     return {
         infoTabButtons={},
         infoTabs={},
-        selectedTab="default",
+        selectedTab="",
         Select = function (self, tabName)
             if self.selectedTab == tabName then
                 return
@@ -25,37 +25,58 @@ function TabManager:Create()
             end
             self.selectedTab = tabName
         end,
-        CreateTabButton = function(self, tabName, parent, tabSelectedText, tabDeselectedText)
-            local tabButton = CreateFrame("BUTTON", "angler-tab-button-"..#self.infoTabButtons+1, parent, "UIPanelButtonTemplate")
-            tabButton:SetSize(120, 20)
+        CreateTabButton = function(self, tabName, parent, iconID, tooltipText)
+            local tabBackground = CreateFrame("FRAME", "angler-tab-"..tabName, parent)
+            tabBackground.selected = false
+            tabBackground:SetSize(50, 50)
+            tabBackground.texture = tabBackground:CreateTexture(nil,'ARTWORK')
+            tabBackground.texture:SetRotation(math.pi * 0.5)
+            tabBackground.texture:SetTexture("Interface\\SPELLBOOK\\SpellBook-SkillLineTab")
+            tabBackground.texture:SetAllPoints()
+
+            local tabButton = CreateFrame("BUTTON", "angler-tab-button-"..tabName, tabBackground, "ItemButtonTemplate")
+            tabButton:SetSize(24, 24)
+            tabButton:SetPoint("TOPLEFT", tabBackground, "TOPLEFT", 9, -23)
+            _G[tabButton:GetName().."NormalTexture"]:SetSize(24*1.662, 24*1.662)
+            tabButton:SetScript("OnEnter", function()
+                GameTooltip:SetOwner(tabButton, "ANCHOR_TOP")
+                GameTooltip:AddLine(tooltipText)
+                GameTooltip:Show()
+            end)
+        
+            tabButton:SetScript("OnLeave", function()
+                GameTooltip:Hide()
+            end)
+            SetItemButtonTexture(tabButton, GetItemIcon(iconID)) 
             tabButton.tabName = tabName
-            tabButton.selected = false
             tabButton:SetText(tabDeselectedText)
             tabButton.highlightTexture = tabButton:CreateTexture(nil,'ARTWORK')
-            tabButton.highlightTexture:SetTexture("Interface\\UNITPOWERBARALT\\MetalBronze_Horizontal_Frame")
-            tabButton.highlightTexture:SetSize(154, 35)
+            tabButton.highlightTexture:SetTexture("Interface\\Transmogrify\\Transmogrify")
+            tabButton.highlightTexture:SetSize(30, 30)
             tabButton.highlightTexture:SetPoint("CENTER", 0, 0)
             tabButton.highlightTexture:SetVertexColor(1.0, 1.0, 1.0, 1.0)
+            tabButton.highlightTexture:SetTexCoord(0.463, 0.553, 0.0, 0.088)
+            tabButton.highlightTexture:SetBlendMode("ADD")
             tabButton.highlightTexture:Hide()
             tabButton:SetScript("OnClick", function()
-                if tabButton.selected then
+                if tabBackground.selected then
                     self:Select("default")
                     return
                 end
                 self:Select(tabButton.tabName)
             end)
-            tabButton.SetSelected = function(self, selected)
+
+            function tabBackground:SetSelected(selected)
                 if selected then
-                    self.selected = true
-                    self:SetText(tabDeselectedText)
-                    self.highlightTexture:Show()
+                    tabBackground.selected = true
+                    tabButton.highlightTexture:Show()
                 else
-                    self.selected = false
-                    self:SetText(tabSelectedText)
-                    self.highlightTexture:Hide()
+                    tabBackground.selected = false
+                    tabButton.highlightTexture:Hide()
                 end
             end
-            return tabButton
+            
+            return tabBackground
         end,
         Register = function(self, tabName, tabButton, tabFrame)
             self.infoTabButtons[tabName] = tabButton
